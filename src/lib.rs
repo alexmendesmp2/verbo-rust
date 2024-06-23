c46a37b69f2773653c4fa0809ff1f97eb8fd3ee5
Alex Mendes <alexmendesmp@outlook.com>
2024-06-23 15:29:34 -0300

    - Modelo Rust 03

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+use std::ffi::{CString, CStr};
+use std::os::raw::c_char;
+
+#[no_mangle]
+pub extern "C" fn call_hsm(param: *const c_char) -> *mut c_char {
+    let param_entrada: String = unsafe { CStr::from_ptr(param) }.to_string_lossy().into_owned();
+    println!("Call HSM! - {}", param_entrada);
+    CString::new("A87B87DF7544CD").unwrap().into_raw()
+}
+
+#[no_mangle]
+pub extern "C" fn save_to_oracle(param: *const c_char) {
+    println!("Call save_to_oracle");
+    println!("Chave salva no Oracle - {}", unsafe { CStr::from_ptr(param) }.to_string_lossy().into_owned());
+}
